da6eb52472968559e129fef4a4b45145a01c24de
bartoszgrams <bartosz.grams@wp.pl>
2023-01-29 20:20:38 +0100

    Obsolete delete

--- src/main/java/binance/webapp/binanceApp/PersonRepository.java
+++
@@ -1,6 +0,0 @@
-//package binance.webapp.binanceApp;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface PersonRepository extends JpaRepository<Person, Long> {
-//}
